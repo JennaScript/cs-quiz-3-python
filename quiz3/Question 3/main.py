639ded5c5827799e378341dad417cced663c6861
Jennalyn Kabiling <jennalynkabiling23@gmail.com>
2020-10-05 19:14:16 -0700

    Add files via upload

---
+++ quiz3/Question 3/main.py
@@ -0,0 +1,15 @@
+# Quiz 3: Question 3
+
+# Part 1
+hats = { "snapback": 10,
+    "beanie": 5,
+    "baseball cap": 3 } 
+
+# Part 2
+hats["weird top hat"] = 1
+
+# Part 3
+hats["snapback"] = 10 + 1
+
+# Part 4
+hats.pop("weird top hat")
